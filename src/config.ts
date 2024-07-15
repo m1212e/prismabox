8b56f280e92ab10c31c20e2551e66b3fbc753948
m1212e <14091540+m1212e@users.noreply.github.com>
2024-07-15 23:35:44 +0200

    ✨ feat: add additionalFieldsPlain option

--- src/config.ts
+++ src/config.ts
@@ -2,35 +2,36 @@ import { Type, type Static } from "@sinclair/typebox";
 import { Value } from "@sinclair/typebox/value";
 
 const configSchema = Type.Object(
-	{
-		output: Type.String({ default: "./prisma/prismabox" }),
-		typeboxImportVariableName: Type.String({ default: "Type" }),
-		typeboxImportDependencyName: Type.String({ default: "@sinclair/typebox" }),
-		additionalProperties: Type.Boolean({ default: false }),
-		inputModel: Type.Boolean({ default: false }),
-		ignoreIdOnInputModel: Type.Boolean({ default: true }),
-		ignoreCreatedAtOnInputModel: Type.Boolean({ default: true }),
-		ignoreUpdatedAtOnInputModel: Type.Boolean({ default: true }),
-		nullableName: Type.String({ default: "__nullable__" }),
-		allowRecursion: Type.Boolean({ default: true }),
-	},
-	{ additionalProperties: false },
+  {
+    output: Type.String({ default: "./prisma/prismabox" }),
+    typeboxImportVariableName: Type.String({ default: "Type" }),
+    typeboxImportDependencyName: Type.String({ default: "@sinclair/typebox" }),
+    additionalProperties: Type.Boolean({ default: false }),
+    inputModel: Type.Boolean({ default: false }),
+    ignoreIdOnInputModel: Type.Boolean({ default: true }),
+    ignoreCreatedAtOnInputModel: Type.Boolean({ default: true }),
+    ignoreUpdatedAtOnInputModel: Type.Boolean({ default: true }),
+    nullableName: Type.String({ default: "__nullable__" }),
+    allowRecursion: Type.Boolean({ default: true }),
+    additionalFieldsPlain: Type.Optional(Type.Array(Type.String())),
+  },
+  { additionalProperties: false }
 );
 
 // biome-ignore lint/suspicious/noExplicitAny: we want to set the default value
 let config: Static<typeof configSchema> = {} as unknown as any;
 
 export function setConfig(input: unknown) {
-	try {
-		Value.Clean(configSchema, input);
-		Value.Default(configSchema, input);
-		config = Value.Decode(configSchema, Value.Convert(configSchema, input));
-		Object.freeze(config);
-	} catch (error) {
-		console.error(Value.Errors(configSchema, input).First);
-		throw error;
-	}
+  try {
+    Value.Clean(configSchema, input);
+    Value.Default(configSchema, input);
+    config = Value.Decode(configSchema, Value.Convert(configSchema, input));
+    Object.freeze(config);
+  } catch (error) {
+    console.error(Value.Errors(configSchema, input).First);
+    throw error;
+  }
 }
 export function getConfig() {
-	return config;
+  return config;
 }
--- src/generators/plain.ts
+++ src/generators/plain.ts
@@ -4,9 +4,9 @@ import { getConfig } from "../config";
 import { generateTypeboxOptions } from "../annotations/options";
 import type { ProcessedModel } from "../model";
 import {
-	isPrimitivePrismaFieldType,
-	stringifyPrimitiveType,
-	type PrimitivePrismaFieldType,
+  isPrimitivePrismaFieldType,
+  stringifyPrimitiveType,
+  type PrimitivePrismaFieldType,
 } from "./primitiveField";
 import { wrapWithArray } from "./wrappers/array";
 import { wrapWithNullable } from "./wrappers/nullable";
@@ -16,92 +16,93 @@ import { wrapWithOptional } from "./wrappers/optional";
 export const processedPlain: ProcessedModel[] = [];
 
 export function processPlain(models: DMMF.Model[] | Readonly<DMMF.Model[]>) {
-	for (const m of models) {
-		const o = stringifyPlain(m);
-		if (o) {
-			processedPlain.push({ name: m.name, stringRepresentation: o });
-		}
-	}
-	Object.freeze(processedPlain);
+  for (const m of models) {
+    const o = stringifyPlain(m);
+    if (o) {
+      processedPlain.push({ name: m.name, stringRepresentation: o });
+    }
+  }
+  Object.freeze(processedPlain);
 }
 
 export function stringifyPlain(data: DMMF.Model, isInputModel = false) {
-	const annotations = extractAnnotations(data.documentation);
-	if (annotations.isHidden || (isInputModel && annotations.isHiddenInput))
-		return undefined;
+  const annotations = extractAnnotations(data.documentation);
+  if (annotations.isHidden || (isInputModel && annotations.isHiddenInput))
+    return undefined;
 
-	const fields = data.fields
-		.map((field) => {
-			const annotations = extractAnnotations(field.documentation);
-			if (annotations.isHidden || (isInputModel && annotations.isHiddenInput))
-				return undefined;
+  const fields = data.fields
+    .map((field) => {
+      const annotations = extractAnnotations(field.documentation);
+      if (annotations.isHidden || (isInputModel && annotations.isHiddenInput))
+        return undefined;
 
-			// ===============================
-			// INPUT MODEL FILTERS
-			// ===============================
-			// if we generate an input model we want to omit certain fields
+      // ===============================
+      // INPUT MODEL FILTERS
+      // ===============================
+      // if we generate an input model we want to omit certain fields
 
-			if (getConfig().ignoreIdOnInputModel && isInputModel && field.isId)
-				return undefined;
-			if (
-				getConfig().ignoreCreatedAtOnInputModel &&
-				isInputModel &&
-				field.name === "createdAt" &&
-				field.hasDefaultValue
-			)
-				return undefined;
-			if (
-				getConfig().ignoreUpdatedAtOnInputModel &&
-				isInputModel &&
-				field.isUpdatedAt
-			)
-				return undefined;
+      if (getConfig().ignoreIdOnInputModel && isInputModel && field.isId)
+        return undefined;
+      if (
+        getConfig().ignoreCreatedAtOnInputModel &&
+        isInputModel &&
+        field.name === "createdAt" &&
+        field.hasDefaultValue
+      )
+        return undefined;
+      if (
+        getConfig().ignoreUpdatedAtOnInputModel &&
+        isInputModel &&
+        field.isUpdatedAt
+      )
+        return undefined;
 
-			if (
-				isInputModel &&
-				(field.name.toLowerCase().endsWith("id") ||
-					field.name.toLowerCase().endsWith("foreign") ||
-					field.name.toLowerCase().endsWith("foreignkey"))
-			) {
-				return undefined;
-			}
+      if (
+        isInputModel &&
+        (field.name.toLowerCase().endsWith("id") ||
+          field.name.toLowerCase().endsWith("foreign") ||
+          field.name.toLowerCase().endsWith("foreignkey"))
+      ) {
+        return undefined;
+      }
 
-			// ===============================
-			// INPUT MODEL FILTERS END
-			// ===============================
+      // ===============================
+      // INPUT MODEL FILTERS END
+      // ===============================
 
-			let stringifiedType = "";
+      let stringifiedType = "";
 
-			if (isPrimitivePrismaFieldType(field.type)) {
-				stringifiedType = stringifyPrimitiveType({
-					fieldType: field.type as PrimitivePrismaFieldType,
-					options: generateTypeboxOptions(annotations),
-				});
-			} else if (processedEnums.find((e) => e.name === field.type)) {
-				// biome-ignore lint/style/noNonNullAssertion: we checked this manually
-				stringifiedType = processedEnums.find(
-					(e) => e.name === field.type,
-				)!.stringRepresentation;
-			} else {
-				return undefined;
-			}
+      if (isPrimitivePrismaFieldType(field.type)) {
+        stringifiedType = stringifyPrimitiveType({
+          fieldType: field.type as PrimitivePrismaFieldType,
+          options: generateTypeboxOptions(annotations),
+        });
+      } else if (processedEnums.find((e) => e.name === field.type)) {
+        // biome-ignore lint/style/noNonNullAssertion: we checked this manually
+        stringifiedType = processedEnums.find(
+          (e) => e.name === field.type
+        )!.stringRepresentation;
+      } else {
+        return undefined;
+      }
 
-			if (field.isList) {
-				stringifiedType = wrapWithArray(stringifiedType);
-			}
+      if (field.isList) {
+        stringifiedType = wrapWithArray(stringifiedType);
+      }
 
-			if (!field.isRequired) {
-				stringifiedType = wrapWithNullable(stringifiedType);
-				if (isInputModel) {
-					stringifiedType = wrapWithOptional(stringifiedType);
-				}
-			}
+      if (!field.isRequired) {
+        stringifiedType = wrapWithNullable(stringifiedType);
+        if (isInputModel) {
+          stringifiedType = wrapWithOptional(stringifiedType);
+        }
+      }
 
-			return `${field.name}: ${stringifiedType}`;
-		})
-		.filter((x) => x) as string[];
+      return `${field.name}: ${stringifiedType}`;
+    })
+    .filter((x) => x) as string[];
 
-	return `${getConfig().typeboxImportVariableName}.Object({${fields.join(
-		",",
-	)}},${generateTypeboxOptions(annotations)})\n`;
+  return `${getConfig().typeboxImportVariableName}.Object({${[
+    ...fields,
+    getConfig().additionalFieldsPlain ?? [],
+  ].join(",")}},${generateTypeboxOptions(annotations)})\n`;
 }
