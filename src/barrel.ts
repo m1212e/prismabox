efcab667f05401a8844da7d65d00730a7e50bffb
m1212e <14091540+m1212e@users.noreply.github.com>
2024-08-11 21:35:32 +0200

    🧼 format & lint

--- src/barrel.ts
+++ src/barrel.ts
@@ -1,5 +1,7 @@
 import { getConfig } from "./config";
 
 export function generateBarrelFile(imports: string[]) {
-  return imports.map((i) => `export * from "./${i}${getConfig().importFileExtension}";`).join("\n");
+  return imports
+    .map((i) => `export * from "./${i}${getConfig().importFileExtension}";`)
+    .join("\n");
 }
